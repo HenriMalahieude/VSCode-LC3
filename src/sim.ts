4c05a7bfcbbfffac3de34f070c0f148cb3da6233
HenriMalahieude <henrimalahieude@gmail.com>
2022-11-09 16:23:36 -0800

    Better promises, but some not kept

--- src/sim.ts
+++ src/sim.ts
@@ -1,21 +1,49 @@
 import * as vscode from 'vscode';
 
-/*async function importModule(moduleName: string):Promise<any>{
-    console.log("importing ", moduleName);
+async function importModule(moduleName: string):Promise<any>{
+    console.log("Importing ", moduleName);
     const importedModule = await import(moduleName);
     console.log("\timported ...");
     return importedModule;
 }
 
 let lc3;
-if (process.platform == 'darwin'){
-	lc3 = await importModule('lc3interface_MAC');
+function setModule(mod: any){
+    lc3 = mod;
+}
+function failedMod(plat: string){
+    console.log("Failed to load " + plat + " version of interface!");
+}
+if (process.platform == 'darwin'){ //Not a big fan of promises
+    importModule('lc3interface_MAC').then(setModule).catch(() => failedMod("Windows"));
 }else if (process.platform == 'win32'){
-	lc3 = await importModule('lc3interface_WIN');
+    importModule('lc3interface_WIN').then(setModule).catch(() => failedMod("Windows"));
 }else if (process.platform == 'linux'){
-	lc3 = await importModule('lc3interface_LIN');
-}*/
+	importModule('lc3interface_LIN').then(setModule).catch(() => failedMod("Windows"));
+}
 
 export function CreateUI(ctx: vscode.ExtensionContext){
-
+    vscode.window.createTreeView('lc3-tools-view', {treeDataProvider: new IconProvider()});
+    vscode.window.registerTreeDataProvider('lc3-tools-view', new IconProvider());
 }
+
+export function AssembleCode(ctx: vscode.ExtensionContext){
+    //this requires the lc3interface
+
+    //lc3.assemble(fileName); //TODO: Determine open file
+    //opens terminal if there is an issue
+}
+
+class IconProvider implements vscode.TreeDataProvider<vscode.TreeItem>{
+    constructor(){}
+
+    getTreeItem(element: vscode.TreeItem): vscode.TreeItem{
+        return element;
+    }
+
+    getChildren(element?: vscode.TreeItem | undefined): vscode.ProviderResult<vscode.TreeItem[]> {
+        //TODO: I need to return two icons that can be clicked
+        return;
+    }
+}
+
