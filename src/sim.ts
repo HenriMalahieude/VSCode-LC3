581c5d261520a9350826218a3709701cd94b3041
HenriMalahieude <henrimalahieude@gmail.com>
2022-11-14 16:40:40 -0800

    Before the Upload

--- src/sim.ts
+++ src/sim.ts
@@ -1,9 +1,9 @@
 import * as vscode from 'vscode';
 
 async function importModule(moduleName: string):Promise<any>{
-    console.log("Importing ", moduleName);
+    console.log("Importing", moduleName);
     const importedModule = await import(moduleName);
-    console.log("\timported ...");
+    console.log("\tImported!");
     return importedModule;
 }
 
@@ -15,14 +15,16 @@ function failedMod(plat: string){
     console.log("Failed to load " + plat + " version of interface!");
 }
 if (process.platform == 'darwin'){ //Not a big fan of promises
-    importModule('lc3interface_MAC').then(setModule).catch(() => failedMod("Windows"));
+    importModule('lc3interface_MAC').then(setModule).catch(() => failedMod("Apple Arm"));
 }else if (process.platform == 'win32'){
     importModule('lc3interface_WIN').then(setModule).catch(() => failedMod("Windows"));
 }else if (process.platform == 'linux'){
-	importModule('lc3interface_LIN').then(setModule).catch(() => failedMod("Windows"));
+	importModule('lc3interface_LIN').then(setModule).catch(() => failedMod("Linux"));
 }
 
 export function CreateUI(ctx: vscode.ExtensionContext){
+    
+
     vscode.window.createTreeView('lc3-tools-view', {treeDataProvider: new IconProvider()});
     vscode.window.registerTreeDataProvider('lc3-tools-view', new IconProvider());
 }
