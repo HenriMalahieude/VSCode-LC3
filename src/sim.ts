f9362a9bbf47b816746cd9c021eb9efa09855dee
HenriMalahieude <henrimalahieude@gmail.com>
2022-12-12 09:35:51 -0800

    Simulator solution was beneath my nose this entire time....

--- src/sim.ts
+++ src/sim.ts
@@ -1,20 +1,15 @@
-import * as fs from 'fs';
+//import * as fs from 'fs';
 import * as vscode from 'vscode';
-import { exec } from 'child_process';
-//const requireFunc = require('util').promisify(require('require-from-string'));
-//const lc3interface = requireFunc(fs.readFileSync('lc3interface', 'utf8'));
-const lc3interface = require("..\\local_modules\\lc3interface");
+import { exec, spawn } from 'child_process';
 
-/*async function importModule(moduleName: string):Promise<any>{
-    console.log("Importing", moduleName);
-    const importedModule = await import(moduleName);
-    console.log("\tImported!");
-    return importedModule;
-}*/
+//Issue: This needs to be recompiled into a newer version of node, or this project needs to use an older version of node
+//const lc3interface = require("..\\local_modules\\lc3interface");
 
 //TODO: Get separate things for windows, linux, and mac
 let executableFolder = "\\src\\lc3tools_WIN\\"; //default
+let output: vscode.OutputChannel = vscode.window.createOutputChannel("LC3-Tools");
 
+//Note: I wish there was a better way to detect operating system. Like detecting Chipset instead.
 if (process.platform == 'darwin'){
     //TODO: MAC Version
     executableFolder = "\\src\\lc3tools_MAC";
@@ -24,36 +19,71 @@ if (process.platform == 'darwin'){
 }
 
 export function AssembleCode(ctx: vscode.ExtensionContext){
+    output.clear();
+    output.show();
+
     let openedWindow = vscode.window.activeTextEditor?.document.uri.fsPath;
     let extensionLocation: any = ctx.extensionUri.fsPath;
 
     if (openedWindow == undefined || extensionLocation == undefined){
         console.log("No file opened?");
+        output.append("No file is opened?");
         return;
-    }else if (!openedWindow.endsWith(".asm")){
+    }else if (!openedWindow.endsWith(".asm") && !openedWindow.endsWith(".bin")){
         console.log("File doesn't have proper extension?");
+        output.append("File isn't a .asm or .bin file?");
         return;
     }
 
     //let entireCommend = extensionLocation + executableFolder + " --print-level=5 " + openedWindow;
     let command  = extensionLocation + executableFolder + "\\assembler.exe --print-level=5 " + openedWindow;
-    console.log(command);
     exec(command, (error, stdout, stderr) => {
         if (error){
             console.log(`error: ${error.message}`);
+            output.append(error.message);
             return;
         }
 
         if (stderr){
             console.log(`stderr: ${stderr}`);
+            output.append(stderr);
             return;
         }
 
-        //TODO: Create a terminal
-        //console.log(`stdout: ${stdout}`);
+        console.log(`stdout: ${stdout}`);
+        output.append(stdout);
     });
 }
 
 export function OpenSimulator(ctx: vscode.ExtensionContext){
-    console.log("TODO: Simulator");
+    output.clear();
+
+    let openedWindow: string | undefined = vscode.window.activeTextEditor?.document.uri.fsPath;
+    let extensionLocation: any = ctx.extensionUri.path;
+
+    if (openedWindow == undefined || extensionLocation == undefined){ //nothing is selected
+        console.log("No file opened?");
+        output.append("No file is opened?");
+        output.show();
+        return;
+    }else if (openedWindow.indexOf("\\") < 0){ //we don't have an active editor selected
+        console.log(openedWindow);
+        output.append("Please open or click on the file/editor you would like to simulate.");
+        output.show();
+        return;
+    }else if (!openedWindow.endsWith(".asm") && !openedWindow.endsWith(".bin")){
+        console.log("File doesn't have proper extension?");
+        output.append("File isn't a .asm or .bin file? \n(Have you tried clicking on it to select it?)\n" + openedWindow);
+        output.show();
+        return;
+    }
+
+    let fileName = openedWindow?.slice(openedWindow?.lastIndexOf("\\")+1, openedWindow?.lastIndexOf("."));
+    let objFile: any = openedWindow?.slice(0, openedWindow.lastIndexOf("\\")) + "\\" + fileName + ".obj";
+
+    //let entireCommend = extensionLocation + executableFolder + " --print-level=5 " + openedWindow;
+    let command = extensionLocation + executableFolder + "\\simulator.exe --log=sim.txt --print-level=8 " + objFile;
+    console.log(command);
+    
+    let proc = spawn(command);
 }
