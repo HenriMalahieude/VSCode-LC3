848bb5cb4695a142c48fffd6e37b3a2869ff6cb7
HenriMalahieude <henrimalahieude@gmail.com>
2023-01-04 13:46:26 -0800

    Starting work on interpreting the simulator

--- src/sim.ts
+++ src/sim.ts
@@ -1,21 +1,61 @@
-//import * as fs from 'fs';
+import * as fs from 'fs';
 import * as vscode from 'vscode';
-import { exec, spawn } from 'child_process';
+import { ChildProcessWithoutNullStreams, exec, Serializable, spawn } from 'child_process';
 
 //Issue: This needs to be recompiled into a newer version of node, or this project needs to use an older version of node
 //const lc3interface = require("..\\local_modules\\lc3interface");
 
 //TODO: Get separate things for windows, linux, and mac
-let executableFolder = "\\src\\lc3tools_WIN\\"; //default
+let executableFolder = "/src/lc3tools_WIN"; //default
 let output: vscode.OutputChannel = vscode.window.createOutputChannel("LC3-Tools");
+
+let proc: ChildProcessWithoutNullStreams | undefined = undefined;
+let sim_webview: vscode.WebviewPanel | undefined = undefined;
 
 //Note: I wish there was a better way to detect operating system. Like detecting Chipset instead.
 if (process.platform == 'darwin'){
     //TODO: MAC Version
-    executableFolder = "\\src\\lc3tools_MAC";
+    executableFolder = "/src/lc3tools_MAC";
 }else if (process.platform == 'linux'){
 	//TODO: Linux Version
-    executableFolder = "\\src\\lc3tools_LIN";
+    executableFolder = "/src/lc3tools_LIN";
+}
+
+function formatForWindows(entry : string) : string{
+    let thing = entry;
+
+
+
+    return thing;
+}
+
+function escapeSpaces(entry : string): string {
+    let thing : string = ""
+    
+    let location : number = entry.indexOf(" ");
+
+    while (location > 0){
+        thing += entry.substring(0, location) + "\\ ";
+        
+        entry = entry.substring(location+1);
+        location = entry.indexOf(" ");
+    }
+    
+
+    return thing + entry
+}
+
+function summonSimWebview(ctx: vscode.ExtensionContext){
+    let simViewHTML = fs.readFileSync(ctx.extensionUri.path + "/src/simview.html");
+    sim_webview = vscode.window.createWebviewPanel(
+        "sim",
+        "LC3-Tools Integrated Simulator",
+        vscode.ViewColumn.One,
+        {}
+    );
+
+    sim_webview.webview.html = simViewHTML.toString();
+    sim_webview.reveal(vscode.ViewColumn.One, false);
 }
 
 export function AssembleCode(ctx: vscode.ExtensionContext){
@@ -73,7 +113,7 @@ export function OpenSimulator(ctx: vscode.ExtensionContext){
         return;
     }else if (!openedWindow.endsWith(".asm") && !openedWindow.endsWith(".bin")){
         console.log("File doesn't have proper extension?");
-        output.append("File isn't a .asm or .bin file? \n(Have you tried clicking on it to select it?)\n" + openedWindow);
+        output.append("File isn't a .asm or .bin file? \n(Have you tried clicking on it/opening the text editor to select it?)\n" + openedWindow);
         output.show();
         return;
     }
@@ -85,5 +125,34 @@ export function OpenSimulator(ctx: vscode.ExtensionContext){
     let command = extensionLocation + executableFolder + "\\simulator.exe --log=sim.txt --print-level=8 " + objFile;
     console.log(command);
     
-    let proc = spawn(command);
+    summonSimWebview(ctx);
+
+    if (!proc){
+        proc = spawn(command);
+
+        proc.addListener("message", (message: Serializable, sendHandle) => {
+            output.clear();
+            output.append("Message: " + message.toString());
+            output.show();
+        })
+
+        proc.stdout.on('data', (chunk) => {
+            console.log(chunk);
+        })
+
+        proc.addListener("close", (code, signal: NodeJS.Signals) => {
+            output.append("Simulator closed with code (" + String(code) + ")\nAnd signal: "  +signal.toString());
+            output.show();
+        })
+
+        proc.addListener("error", (err: Error) => {
+            output.append("Simulator error: " + err.message);
+        })
+    }
+}
+
+export function CloseSimulator(ctx : vscode.ExtensionContext){
+    proc?.send("quit");
+    proc?.kill();
+    proc = undefined;
 }
