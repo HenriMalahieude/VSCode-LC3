c93584474ddbf7c656290efc30aa8533f1a287b1
HenriMalahieude <henrimalahieude@gmail.com>
2023-04-07 16:18:57 -0700

    Some edits made to test file

--- test/test.asm
+++ test/test.asm
@@ -1,10 +1,24 @@
 .orig x3000
 
-LD R0, x0
+AND R0, R0, #0
 
+;Simple multiplication by 2
 ADD R0, R0, #1
 ADD R0, R0, R0
 ADD R0, R0, R0
+
+;Put it back to zero
+AND R0, R0, #0
+
+;This will be testing the NOT
+ADD R0, R0, #5
+
+;Negate the number
+NOT R0, R0
+ADD R0, R0, #1
+
+;This should result in zero
+ADD R0, R0, #5
 
 HALT
 .end
