18ebf4e6902efd8d149a1f5dd4756b9631c3fff8
HenriMalahieude <henrimalahieude@gmail.com>
2023-04-16 11:37:06 -0700

    Preprocessing Done, JMP, fixed BR detection, Warnings

--- src/Simulator.ts
+++ src/Simulator.ts
@@ -8,42 +8,43 @@ export interface Result{
 	message?: string;
 }
 
-export interface LC3Data{
-	assembly: string | undefined;
-	machine: number;
+interface Bit16Location{
+	fileIndex: number;
+	pc: number;
 }
 
-interface LC3Label{
-	label: string;
-	fileLine: number;
-	location: number;
-	details: LC3Data;
+interface LC3Data{
+	assembly: string | undefined;
+	machine: number;
+	location: Bit16Location;
 }
 
 const emptyLC3Data: LC3Data = { //For Memory Traversal TODO
 	assembly: "",
-	machine: 0b0000000000000000
+	machine: 0b0000000000000000,
+	location: {fileIndex: 0, pc: 0x3000},
 }
 
-//TODO: Initialize PC to first .ORIGIN Encountered
+//TODO: Initialize PC to first .ORIG Encountered
 export class LC3Simulator extends EventEmitter{
 	status: Result = {success: true};
 	halted: boolean = false;
 
 	registers: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
-	memory: Map<number, LC3Data | LC3Label>;
+	memory: Map<number, LC3Data>;
 	pc: number = 0x2FFF;
 	psr: number = 0;
 	mcr: number = 0;
 
 	public file: vscode.TextDocument;
 	private currentLine: number = -1;
+	private processed: boolean = false;
 
-	//Map .ORIGIN x#### locations to file line numbers (for return/jumping/conditionals)
+	//Map .ORIG x#### locations to file line numbers (for return/jumping/conditionals)
 	private subroutineLocations: Map<number, number>;
 
 	//Map Labels to locations in memory
-	private labelLocations: Map<string, number>; //For variables, all the way to positional labels
+	private labelLocations: Map<string, Bit16Location>; //For variables, all the way to positional labels
 
 	constructor(f: vscode.TextDocument){
 		super();
@@ -53,14 +54,21 @@ export class LC3Simulator extends EventEmitter{
 		//Initialize the Object
 		this.file = f;
 		this.subroutineLocations = new Map<number, number>();
-		this.labelLocations = new Map<string, number>();
+		this.labelLocations = new Map<string, Bit16Location>();
 
-		console.log("Opened and simulating: " + this.file.fileName)
-		this.status = this.preprocess()
+		console.log("Opened and ready to simulate: " + this.file.fileName)
+	}
+
+	public InitializeSimulator(){
+		if (this.halted || this.processed) return;
+
+		this.status = this.preprocess();
 		if (!this.status.success){
 			this.halted = true;
 			this.status.context = "Preprocessing"
 		}
+
+		this.processed = true;
 	}
 
 	//-----------------------Public Functions for Simulator Control-----------------------
@@ -131,144 +139,186 @@ export class LC3Simulator extends EventEmitter{
 
 	private preprocess(): Result{
 		let currentLocation: number = -1; //Sentinel Number
-		let codeAllowed: boolean = false; //To restrict code to between .ORIGIN and .END
+		let codeAllowed: boolean = false; //To restrict code to between .ORIG and .END
 		let subroutineMark: boolean = false; //To be able to use the subroutineLocations property properly
 
-		for (let i = 0; i < this.file.lineCount; i++){
-			let lineOfText = this.file.lineAt(i)
+		for (let i = 1; i-1 < this.file.lineCount; i++){
+			let lineOfText = this.file.lineAt(i-1)
 			let txt = lineOfText.text.trim().toLocaleUpperCase();
 			let command = txt.split(" ");
 
 			if (lineOfText.isEmptyOrWhitespace || txt.substring(0, 1) == ";") continue; //Ignore Empty Space and Comments
 
 			//The start of routine/subroutine
-			if (txt.startsWith(".ORIGIN")){
-				if (!codeAllowed){
-					currentLocation = this.convertNumber(command[1]);
-					subroutineMark = true;
-					codeAllowed = true;
+			if (txt.startsWith(".ORIG ") && command.length == 2){
+				if (command.length == 2){
+					if (!codeAllowed){
+						currentLocation = this.convertNumber(command[1]);
+						subroutineMark = true;
+						codeAllowed = true;
+						if (Number.isNaN(currentLocation)){
+							return {success: false, message: "Could not convert number supplied to a valid location in memory.\n(Location must be specified in hex (x))", line: i};
+						}else if (currentLocation < 0x3000 || currentLocation > 0xFE00){
+							return {success: false, message: "Cannot populate program within system reserved memory\n(Reserved memory [0x0000 -> 0x3000) && (0xFE00 -> 0xFFFF])", line: i};
+						}
 	
-					if (Number.isNaN(currentLocation)){
-						return {success: false, message: "Could not convert number supplied to a valid location in memory.\n(Location must be specified in hex (x))", line: i};
-					}else if (currentLocation < 0x3000 || currentLocation > 0xFE00){
-						return {success: false, message: "Cannot populate program within system reserved memory\n(Reserved memory [0x0000 -> 0x3000) && (0xFE00 -> 0xFFFF])", line: i};
+						if (command[1].startsWith("B") || command[1].startsWith("#")) this.emit("warning", {success: false, line: i, message: ".ORIG pseudo ops should provide number in hexdecimal format (x).", context: "Preprocessing Warning"})
+					}else{
+						return {success: false, message: "Did not specify where previous routine op codes ended. Please use '.end' to specify.", line: i};
 					}
 				}else{
-					return {success: false, message: "Did not specify where program op codes ended. Please use '.end' to specify.", line: i};
+					return {success: false, line: i, message: "Incorrect amount of parameters given to .ORIG pseudo\n(Format expected: .ORIG x3000)"};
 				}
-
+				
 				continue;
 			}
 			
 			//End of area where code is allowed
-			if (txt.startsWith(".END")){
+			if (txt.match(/.END\s*/gm)){
 				if (codeAllowed){
 					codeAllowed = false;
 				}else{
-					return {success: false, message: ".END missing corresponding .ORIGIN pseudo-op before it.", line: i};
+					return {success: false, message: ".END missing corresponding .ORIG pseudo-op before it.", line: i};
 				}
 				
 				continue;
 			}
 
 			//Variables
-			if (txt.match(/.FILL\s*/gm) || txt.match(/.STRINGZ\s*/gm) || txt.match(/.BLKW\s*/gm)){
+			if (txt.match(/\s+.FILL\s+/gm) || txt.match(/\s+.STRINGZ\s+/gm) || txt.match(/\s+.BLKW\s+/gm)){
 				//TODO: Create a warning system that informs users of this
-				if (codeAllowed) this.emit("warning", {success: false, message: "Please avoid placing .FILL, .STRINGZ, or .BLKW before .END statement.", line: i, context: "Variable Preprocessing"});
+				if (codeAllowed) {
+					this.emit("warning", {success: false, message: "Please avoid placing .FILL, .STRINGZ, or .BLKW before .END statement.", line: i, context: "Preprocessing Warning"});
+				}
+
+				if (currentLocation <= -1) return {success: false, line: i, message: "Don't know where label is labelling."};
 
 				if (command.length < 3) return {success: false, message: ".FILL, .STRINGZ, or .BLKW incorrectly formatted.", line: i};
 
 				if (this.startsWithCommand(txt)) return {success: false, message: "Cannot start .FILL, .STRINGZ, or .BLKW with opcode", line: i};
 
-				if (txt.match(/.FILL\s*/gm)){ //Single Variables
-					let numerical = this.convertNumber(command[3]);
+				if (txt.match(/\s+.FILL\s+/gm)){ //Single Variables
+					let numerical = this.convertNumber(command[2]);
 					if (Number.isNaN(numerical)) return {success: false, line: i, message: "Number provided is not hex (x), bin (b), or decimal (#)"};
 
-					let ll: LC3Label = {
-						fileLine: i,
-						label: command[0],
-						location: currentLocation,
-						details: {
-							assembly: txt,
-							machine: numerical,
-						}
+					let ll: LC3Data = { 
+						assembly: txt,
+						machine: numerical,
+						location: {pc: currentLocation, fileIndex: i-1},
 					}
 	
 					this.memory.set(currentLocation, ll);
-					this.labelLocations.set(command[0], currentLocation);
-				} else if (txt.match(/.STRINGZ\s*/gm)){ //Strings
-					let ll: LC3Label = {
-						fileLine: i,
-						label: command[0],
-						location: currentLocation,
-						details: {
-							assembly: txt,
-							machine: command[2].charCodeAt(0),
-						}
+					this.labelLocations.set(command[0], {pc: currentLocation, fileIndex: i-1});
+				} else if (txt.match(/\s+.STRINGZ\s+/gm)){ //Strings
+					let ss = command[2].replaceAll(/"/gm, "");
+					let ll: LC3Data = {
+						assembly: ss.at(0),
+						machine: ss.charCodeAt(0),
+						location: {pc: currentLocation, fileIndex: i-1},
 					}
 
-					this.memory.set(currentLocation, ll);
-					this.labelLocations.set(command[0], currentLocation)
+					//console.log(ss, ll, currentLocation);
 
-					for (let i = 1; i < command[2].length; i++){
+					this.memory.set(currentLocation, ll);
+					this.labelLocations.set(command[0], {pc: currentLocation, fileIndex: i-1})
+
+					for (let j = 1; j < ss.length; j++){
 						currentLocation +=1; //It's fine to put it before hand, because outside the "if" it will increase
-						this.memory.set(currentLocation, {assembly: command[2].at(i), machine: command[2].charCodeAt(i)})
+						this.memory.set(currentLocation, 
+							{
+								assembly: ss.at(j),
+								machine: ss.charCodeAt(j),
+								location: {pc: currentLocation, fileIndex: -1},
+							})
 					}
 					currentLocation += 1;
-					this.memory.set(currentLocation, {assembly: "\0", machine: 0}) //Remembering the zero section
-
-				} else if (txt.match(/.BLKW\s*/gm)){ //Array/Blocking
-					let ll: LC3Label = {
-						fileLine: i,
-						label: command[0],
-						location: currentLocation,
-						details: {
-							assembly: txt,
+					this.memory.set(currentLocation, 
+						{
+							assembly: "\0",
 							machine: 0,
-						}
-					}
+							location: {pc: currentLocation, fileIndex: -1},
+						}) //Remembering the zero section
 
+				} else if (txt.match(/\s+.BLKW\s+/gm)){ //Array/Blocking
+					let ll: LC3Data = {
+						assembly: command[0] + " BLKW 0",
+						machine: 0,
+						location: {pc: currentLocation, fileIndex: i-1},
+					}
+					
 					this.memory.set(currentLocation, ll);
-					this.labelLocations.set(command[0], currentLocation);
+					this.labelLocations.set(command[0], {pc: currentLocation, fileIndex: i-1});
 
 					let total = Number(command[2]);
 					if (Number.isNaN(total)) return {success: false, line: i, message: "Could not convert number (NaN err). Do not use any specifiers (x,b,#) as this is implicitly converted from decimal."};
 
-					for (let i = 1; i < Number(command[2]); i++){
+					for (let j = 1; j < Number(command[2]); j++){
 						currentLocation += 1;
-						this.memory.set(currentLocation, {assembly: "", machine: 0});
+						this.memory.set(currentLocation, 
+							{
+								assembly: command[0] + " BLKW " + String(j),
+								machine: 0,
+								location: {pc: currentLocation, fileIndex:i-1},
+							});
 					}
 				}
 
-				currentLocation +=1;
+				currentLocation += 1;
 				continue;
 			}
 
-			if (!this.startsWithCommand(txt)){
-				let ll: LC3Label = {
-					fileLine: i,
-					location: currentLocation,
-					label: command[0],
-					details: {
-						assembly: txt,
-						machine: 0,
+			if (currentLocation > 0x2FFF){
+				if (!this.startsWithCommand(txt)){
+					/*let ll: LC3Label = {
+						fileLine: i,
+						location: currentLocation,
+						label: command[0],
+						details: {
+							assembly: txt,
+							machine: 0,
+						}
+					}*/
+
+					//console.log(command.length, command[1].substring(0,1), this.startsWithCommand(command[1]+" "), command[1]);
+
+					if (command.length > 1 && command[1].substring(0, 1) != ";" && this.startsWithCommand(command[1]+" ")){
+						this.memory.set(currentLocation, 
+							{
+								assembly: txt,
+								machine: 0,
+								location: {pc: currentLocation, fileIndex:i-1},
+							})//TODO
+					}else if (command.length > 1 && command[1].substring(0, 1) != ";"){
+						return {success: false, line: i, message: "Positional label properly labeled, but did not understand following command on same line."};
 					}
-				}
 
-				if (command.length > 1 && command[1].substring(0, 1) != ";" && this.startsWithCommand(command[1])){
-					currentLocation += 1; //So that it doesn't get overwritten.
-				}else if (command.length > 1 && command[1].substring(0, 1) != ";"){
-					return {success: false, line: i, message: "Positional label properly labeled, but did not understand following command on same line."};
+					this.labelLocations.set(command[0], {pc: currentLocation, fileIndex: i-1});
+					
+					if (command.length > 1 && command[1].substring(0, 1) != ";" && this.startsWithCommand(command[1]+" ")){
+						currentLocation += 1
+					}
+					continue;
+				}else if (subroutineMark){ //If this line has a command, and we've just arrived from a .ORIG pseudo
+					this.subroutineLocations.set(currentLocation, i);
+					subroutineMark = false;
 				}
-
-				continue;
-			}else if (subroutineMark){ //If this line has a command, and we've just arrived from a .ORIGIN pseudo
-				this.subroutineLocations.set(currentLocation, i);
-				subroutineMark = false;
+			}else{
+				return {success: false, line: i, message: "Missing location to place opcodes at. (.ORIG x3000?)"};
 			}
+			
+			this.memory.set(currentLocation, 
+				{
+					assembly: txt,
+					machine: 0,
+					location: {pc: currentLocation, fileIndex:i-1},
+				})
 
-			currentLocation += 1;
+			currentLocation +=1;
 		}
+
+		//console.log(this.memory);
+		//console.log(this.labelLocations);
+		//console.log(this.subroutineLocations);
 
 		return {success: true};
 	}
@@ -310,7 +360,7 @@ export class LC3Simulator extends EventEmitter{
 		}else if (manip.startsWith("BR ")){
 			return {success: false, message: "TODO"}; //TODO
 		}else if (manip.startsWith("JMP ")){
-			return {success: false, message: "TODO"}; //TODO
+			return this.JMP(manip);
 		}else if (manip.startsWith("JSR ")){
 			return {success: false, message: "TODO"}; //TODO
 		}else if (manip.startsWith("JSRR ")){
@@ -440,26 +490,46 @@ export class LC3Simulator extends EventEmitter{
 
 		return {success: true};
 	}
+
+	private JMP(line: string): Result{
+		let command = line.split(" ");
+		let destination = command[1];
+
+		let destIndex = Number(destination.substring(1,2));
+		if (!destination.startsWith("R") || Number.isNaN(destIndex) || destIndex < 0 || destIndex > 7){
+			return {success: false, message: "First Source Register is NaN or out of bounds."}
+		}
+
+		let whereTo:LC3Data | undefined = this.memory.get(this.registers[destIndex]);
+		if (whereTo === undefined){
+			return {success: false, message: "Attempting to jump to unregistered location in memory. Forcing simulation end."};
+		}else{
+			this.pc=whereTo.location.pc;
+			this.currentLine = whereTo.location.fileIndex;
+			
+			//NOTE: I don't know how to make the JMP properly execute and not mess up flow without this. (TODO way later)
+			this.interpretCommand(this.file.lineAt(this.currentLine).text);
+		}
+
+		return {success: true};
+	}
 	
 	private LD(line: string): Result{
 		let command = line.split(" ");
 		let destinationS = command[1].substring(1, 2);
-		let numerical = this.convertNumber(command[2]);
-
-		console.log(destinationS, numerical)
 		
 		let registerIndex = Number(destinationS);//Get R#
 		if (!command[1].startsWith("R") || Number.isNaN(registerIndex) || registerIndex > 7 || registerIndex < 0){
 			return {success: false, message: "Destination Register is NaN or out of bounds."};
 		}
 
-		if (Number.isNaN(numerical)){
-			//TODO: Labels
-			return {success: false, message: "Source is NaN or out of signed 9-bit bounds."};
+		let addr = this.labelLocations.get(command[2]);
+		if (!addr){
+			return {success: false, message: "Attempted to get locate non-existent label."}
 		}
+		let numerical = this.memory.get(addr.pc);
 
-		//this.registers[registerIndex] = numerical;
-		//TODO: Look in Memory
+		this.registers[registerIndex] = numerical ? numerical.machine : 0;
 
 		return {success: true};
 	}
@@ -506,7 +576,7 @@ export class LC3Simulator extends EventEmitter{
 		if (line.startsWith("ADD ")) return true;
 		if (line.startsWith("AND ")) return true;
 
-		if (line.startsWith("BR ")) return true;
+		if (line.match(/BR[|nzp]/g)) return true;
 		if (line.startsWith("JMP ")) return true;
 		if (line.startsWith("JSR ")) return true;
 		if (line.startsWith("JSRR ")) return true;
@@ -540,5 +610,9 @@ export class LC3Simulator extends EventEmitter{
 		}
 
 		return line;
+	}
+
+	private convertCommandToMachine(line: string): number{
+		return 0b00;
 	}
 }
