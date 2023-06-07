464b901d3020c3894b44afe64f7500c35b661cb4
HenriMalahieude <henrimalahieude@gmail.com>
2023-06-06 17:20:19 -0700

    Skeleton Adapter for Grader

---
+++ src/Grader/DebugAdapter.ts
@@ -0,0 +1,50 @@
+import { DebugProtocol } from "@vscode/debugprotocol/lib/debugProtocol";
+import * as DAP from "@vscode/debugadapter";
+
+interface ILaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
+	/** An absolute path to the "program" to debug. */
+	program: string;
+}
+
+export class LC3GraderAdapter extends DAP.DebugSession {
+	constructor(){
+		super();
+
+		this.setDebuggerColumnsStartAt1(false);
+		this.setDebuggerLinesStartAt1(false);
+	}
+
+	protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {}
+
+	protected disconnectRequest(response: DebugProtocol.DisconnectResponse, args: DebugProtocol.DisconnectArguments, request?: DebugProtocol.Request): void {}
+
+	protected async attachRequest(response: DebugProtocol.AttachResponse, args: ILaunchRequestArguments) {}
+
+	protected launchRequest(response: DebugProtocol.LaunchResponse, args: ILaunchRequestArguments) {}
+
+	protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {}
+	
+	protected stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments, request?: DebugProtocol.Request | undefined): void {}
+
+	protected cancelRequest(response: DebugProtocol.CancelResponse, args: DebugProtocol.CancelArguments, request?: DebugProtocol.Request | undefined): void {}
+
+	protected terminateRequest(response: DebugProtocol.TerminateResponse, args: DebugProtocol.TerminateArguments, request?: DebugProtocol.Request | undefined): void {}
+
+	protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {}
+
+	protected variablesRequest(response: DebugProtocol.VariablesResponse, args: DebugProtocol.VariablesArguments, request?: DebugProtocol.Request): void {}
+
+	protected setVariableRequest(response: DebugProtocol.SetVariableResponse, args: DebugProtocol.SetVariableArguments): void {}
+
+	protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments, request?: DebugProtocol.Request | undefined): void {}
+
+	protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {}
+
+	protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {}
+
+	protected stepInRequest(response: DebugProtocol.StepInResponse, args: DebugProtocol.StepInArguments): void {}
+
+	protected stepOutRequest(response: DebugProtocol.StepOutResponse, args: DebugProtocol.StepOutArguments, request?: DebugProtocol.Request | undefined): void {}
+
+	protected customRequest(command: string, response: DebugProtocol.Response, args: any) {}
+}
