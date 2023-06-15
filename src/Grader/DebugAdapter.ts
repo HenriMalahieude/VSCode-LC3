2a7e23dc48f409ed5415fa49849d7e8773a2c089
HenriMalahieude <henrimalahieude@gmail.com>
2023-06-15 10:40:40 -0700

    Get and Set Variables, limited to setting registers

--- src/Grader/DebugAdapter.ts
+++ src/Grader/DebugAdapter.ts
@@ -22,6 +22,9 @@ export class LC3GraderAdapter extends DAP.DebugSession {
 	private grader: CLIInterface;
 
 	private valuesInHex: boolean = true;
+	private memoryCount: number = 16;
+
+	private memoryHead: number = -1;
 
 	constructor(ctx: vscode.ExtensionContext, otc: vscode.OutputChannel){
 		super();
@@ -97,7 +100,7 @@ export class LC3GraderAdapter extends DAP.DebugSession {
 		this.sendResponse(response);
 	}
 
-	protected cancelRequest(response: DebugProtocol.CancelResponse, args: DebugProtocol.CancelArguments | undefined): void {} //TODO
+	protected cancelRequest(response: DebugProtocol.CancelResponse, args: DebugProtocol.CancelArguments | undefined) {}
 
 	protected terminateRequest(response: DebugProtocol.TerminateResponse, args: DebugProtocol.TerminateArguments | undefined): void {
 		this.grader.CloseDebuggerCLI();
@@ -114,18 +117,111 @@ export class LC3GraderAdapter extends DAP.DebugSession {
 		this.sendResponse(response);
 	}
 
-	protected variablesRequest(response: DebugProtocol.VariablesResponse, args: DebugProtocol.VariablesArguments): void {} //TODO
-
-	protected async setVariableRequest(response: DebugProtocol.SetVariableResponse, args: DebugProtocol.SetVariableArguments) {} //TODO
-
 	protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments | undefined) {
 		response.body = {
 			scopes: [
-				{name: "Registers", variablesReference: 1, expensive: false},
-				{name: "Memory", variablesReference: 2, expensive: false},
+				{name: "Registers", variablesReference: 1, expensive: true},
+				{name: "Memory", variablesReference: 2, expensive: true},
 			]
 		};
 		
+		this.sendResponse(response);
+	}
+
+	protected async variablesRequest(response: DebugProtocol.VariablesResponse, args: DebugProtocol.VariablesArguments) {
+		console.log("Update Variables " + args.variablesReference.toString())
+		let registers = await this.grader.GetRegisters();
+		if (registers.value == undefined || registers.message != undefined){
+			return this.graderError(response, registers.message ? registers.message : "Register Get Default Error?");
+		}
+		
+		if (args.variablesReference == 1) { //Registers section
+			response.body = {
+				variables: [
+					{type: "string", name: "R0", value: this.formatNumber(registers.value[0]), variablesReference: 0},
+					{type: "string", name: "R1", value: this.formatNumber(registers.value[1]), variablesReference: 0},
+					{type: "string", name: "R2", value: this.formatNumber(registers.value[2]), variablesReference: 0},
+					{type: "string", name: "R3", value: this.formatNumber(registers.value[3]), variablesReference: 0},
+					{type: "string", name: "R4", value: this.formatNumber(registers.value[4]), variablesReference: 0},
+					{type: "string", name: "R5", value: this.formatNumber(registers.value[5]), variablesReference: 0},
+					{type: "string", name: "R6", value: this.formatNumber(registers.value[6]), variablesReference: 0},
+					{type: "string", name: "R7", value: this.formatNumber(registers.value[7]), variablesReference: 0},
+					{type: "string", name: "PC", value: this.formatNumber(registers.value[8]), variablesReference: 0},
+					{type: "string", name: "PSR", value: this.formatNumber(registers.value[9]), variablesReference: 0},
+					{type: "string", name: "MCR", value: this.formatNumber(registers.value[10]), variablesReference: 0},
+				]
+			}
+		}else if (args.variablesReference == 2){ //Memory Section
+			response.body = {
+				variables: []
+			}
+
+			let memoryTop = registers.value[8];
+			let start = "(PC) 0x"
+			if (this.memoryHead != -1){
+				memoryTop = this.memoryHead;
+				start = "0x";
+			}
+
+			console.log(memoryTop)
+
+			response.body.variables.push({type: "string", name: "Memory Start", value: start + memoryTop.toString(16), variablesReference: 0});
+			
+			let range = await this.grader.GetMemoryRange(memoryTop, this.memoryCount);
+			if (range.message != undefined || range.value == undefined) return this.graderError(response, range.message ? range.message : "Memory Get Default Error?");
+
+			for (let i = 0; i < this.memoryCount; i++){
+				let v = range.value.get(memoryTop+i);
+				let vv: string = v ? v : "?";
+
+				response.body.variables.push({type: "string", name: "0x"+(memoryTop + i).toString(16), value: vv, variablesReference: 0});
+			}
+		}
+
+		this.sendResponse(response);
+	}
+
+	protected async setVariableRequest(response: DebugProtocol.SetVariableResponse, args: DebugProtocol.SetVariableArguments) {
+		if (args.name.startsWith("R")){
+			let val = Number(args.value);
+			if (!Number.isNaN(val)){
+				let succ = await this.grader.SetRegisters(Number(args.name.substring(1,2)), val);
+
+				if (!succ){
+					return this.graderError(response, "Couldn't set register value. Is Debugger running?");
+				}
+
+				response.body = {value: this.formatNumber(val)};
+			}
+		}else if (args.name == "PC" || args.name == "PSR" || args.name == "MCR"){
+			return this.graderError(response, "Cannot Edit System Reserved Registers");
+		}else if (args.name.startsWith("0x")){
+			return this.graderError(response, "Editing Memory is not Supported in Grader Mode"); //Can be changed, but we want the students using the simulator
+		}else if (args.name == "Memory Start"){
+			let location = Number(args.value);
+			console.log(location)
+			if (!Number.isNaN(location)){
+				if (location > 0xFFFF || location < 0){
+					this.memoryHead = -1;
+
+					let register = await this.grader.GetRegisters();
+					if (register.message != undefined || register.value == undefined) return this.graderError(response, "Couldn't get registers?");
+
+					response.body = {value: "(PC) 0x" + register.value[8].toString(16)};
+				}else{
+					response.body = {value: "0x"+location.toString(16)};
+
+					if (this.memoryHead == -1){ //Only send this message once
+						this.outputChannel.appendLine("To return to Auto Memory View, edit \"Memory Start\" variable to -1");
+					}
+
+					this.memoryHead = location;
+				}
+
+				this.sendEvent(new DAP.StoppedEvent("memory update", 1)); //This is so it updates the memory
+			}
+		}
+
 		this.sendResponse(response);
 	}
 
@@ -147,6 +243,18 @@ export class LC3GraderAdapter extends DAP.DebugSession {
 	}
 
 	//Helper
+	private formatNumber(x: number) {
+		if (this.valuesInHex){
+			let nn = x;
+			if (nn < 0){
+				nn = 0xFFFF + x + 1; //Since it's negative we don't want 0x-5, we want 0xFFFB;
+			}
+			return ('0x' + nn.toString(16).toLocaleUpperCase());
+		}
+
+		return x.toString(10);
+	}
+
 	private graderError(response: DebugProtocol.Response, message: string){
 		return this.sendErrorResponse(response, {
 			id: 1002,
